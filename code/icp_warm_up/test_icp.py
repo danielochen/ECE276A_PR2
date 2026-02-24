ef526bfcd72a4d8f47df431fa2d4a03ddeea89cb
dychen0742 <38592230+danielochen@users.noreply.github.com>
2026-02-23 23:17:00 -0800

    part 1 done

--- code/icp_warm_up/test_icp.py
+++ code/icp_warm_up/test_icp.py
@@ -4,7 +4,7 @@ from utils import read_canonical_model, load_pc, visualize_icp_result
 
 if __name__ == "__main__":
   obj_name = 'drill' # drill or liq_container
-  num_pc = 4 # number of point clouds
+  num_pc = 50 # number of point clouds
 
   source_pc = read_canonical_model(obj_name)
 
