f720f63d6c5ff49a0f57e148a3cb00d9c0703439
dychen0742 <38592230+danielochen@users.noreply.github.com>
2026-02-24 02:02:39 -0800

    added lidar mapping

--- code/load_data.py
+++ code/load_data.py
@@ -8,26 +8,26 @@ def load_dataset(dataset=20):
   kinect = {}
   
   with np.load("../data/Encoders%d.npz"%dataset) as data:
-    encoder["encoder_counts"] = data["counts"] # 4 x n encoder counts
-    encoder["encoder_stamps"] = data["time_stamps"] # encoder time stamps
+    encoder["encoder_counts"] = data["counts"]                                  # 4 x n encoder counts
+    encoder["encoder_stamps"] = data["time_stamps"]                             # encoder time stamps
 
   with np.load("../data/Hokuyo%d.npz"%dataset) as data:
-    lidar["lidar_angle_min"] = data["angle_min"] # start angle of the scan [rad]
-    lidar["lidar_angle_max"] = data["angle_max"] # end angle of the scan [rad]
-    lidar["lidar_angle_increment"] = data["angle_increment"] # angular distance between measurements [rad]
-    lidar["lidar_range_min"] = data["range_min"] # minimum range value [m]
-    lidar["lidar_range_max"] = data["range_max"] # maximum range value [m]
-    lidar["lidar_ranges"] = data["ranges"]       # range data [m] (Note: values < range_min or > range_max should be discarded)
-    lidar["lidar_stamsp"] = data["time_stamps"]  # acquisition times of the lidar scans
+    lidar["lidar_angle_min"] = data["angle_min"]                                # start angle of the scan [rad]
+    lidar["lidar_angle_max"] = data["angle_max"]                                # end angle of the scan [rad]
+    lidar["lidar_angle_increment"] = data["angle_increment"]                    # angular distance between measurements [rad]
+    lidar["lidar_range_min"] = data["range_min"]                                # minimum range value [m]
+    lidar["lidar_range_max"] = data["range_max"]                                # maximum range value [m]
+    lidar["lidar_ranges"] = data["ranges"]                                      # range data [m] (Note: values < range_min or > range_max should be discarded)
+    lidar["lidar_stamps"] = data["time_stamps"]                                 # acquisition times of the lidar scans
     
   with np.load("../data/Imu%d.npz"%dataset) as data:
-    imu["imu_angular_velocity"] = data["angular_velocity"] # angular velocity in rad/sec
-    imu["imu_linear_acceleration"] = data["linear_acceleration"] # accelerations in gs (gravity acceleration scaling)
-    imu["imu_stamps"] = data["time_stamps"]  # acquisition times of the imu measurements
+    imu["imu_angular_velocity"] = data["angular_velocity"]                      # angular velocity in rad/sec
+    imu["imu_linear_acceleration"] = data["linear_acceleration"]                # accelerations in gs (gravity acceleration scaling)
+    imu["imu_stamps"] = data["time_stamps"]                                     # acquisition times of the imu measurements
   
   with np.load("../data/Kinect%d.npz"%dataset) as data:
-    kinect["disp_stamps"] = data["disparity_time_stamps"] # acquisition times of the disparity images
-    kinect["rgb_stamps"] = data["rgb_time_stamps"] # acquisition times of the rgb images
+    kinect["disp_stamps"] = data["disparity_time_stamps"]                       # acquisition times of the disparity images
+    kinect["rgb_stamps"] = data["rgb_time_stamps"]                              # acquisition times of the rgb images
 
     return encoder, lidar, imu, kinect
 
--- code/pr2_utils.py
+++ code/pr2_utils.py
@@ -2,6 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt; plt.ion()
 import time
+from load_data import load_dataset
 
 def tic():
   return time.time()
@@ -70,7 +71,7 @@ def test_bresenham2D():
   start_time = time.time()
   for i in range(0,num_rep):
 	  x,y = bresenham2D(sx, sy, 500, 200)
-  print("1000 raytraces: --- %s seconds ---" % (time.time() - start_time))
+  # print("1000 raytraces: --- %s seconds ---" % (time.time() - start_time))
 
 
 def show_lidar():
@@ -90,7 +91,20 @@ def show_lidar():
 def plot_map(mapdata, cmap="binary"):
   plt.imshow(mapdata.T, origin="lower", cmap=cmap)
 
-def test_map():
+def test_map(dataset=20):
+  t_start, t_end, encoder_counts, encoder_t, v, imu_wz, imu_t, x, y, encoder_theta = encoder_IMU_odometry(dataset)
+  encoder_data, lidar_data, imu_data, kinect_data = load_dataset(dataset)
+
+  angle_min = lidar_data["lidar_angle_min"]                                     # start angle of the scan [rad]
+  angle_max = lidar_data["lidar_angle_max"]                                     # end angle of the scan [rad]
+  angle_increment = lidar_data["lidar_angle_increment"]                         # angular distance between measurements [rad]
+  range_min = lidar_data["lidar_range_min"]                                     # minimum range value [m]
+  range_max = lidar_data["lidar_range_max"]                                     # maximum range value [m]
+  lidar_ranges = lidar_data["lidar_ranges"]                                     # range data [m] (Note: values < range_min or > range_max should be discarded)
+  lidar_stamps = lidar_data["lidar_stamps"]                                     # acquisition times of the lidar scans
+
+  # need to transform the lidar measurements bsaed off the 
+  print(lidar_ranges.shape[0])
   # Initialize a grid map
   MAP = {}
   MAP['res'] = np.array([0.05, 0.05])    # meters
@@ -102,35 +116,173 @@ def test_map():
   MAP['map'] = np.zeros(MAP['size'])
   
   # Load Lidar scan
-  ranges = np.load("test_ranges.npy")
-  angles = np.arange(-135,135.25,0.25)*np.pi/180.0
-  valid1 = np.logical_and((ranges < 30),(ranges> 0.1))
+  print("lidar_ranges shape:", lidar_ranges.shape)
+  n_rays, n_scans = lidar_ranges.shape
+  # angles = np.arange(-135,135.25,0.25)*np.pi/180.0
+  angles = (angle_min + np.arange(n_rays) * angle_increment).reshape(-1)
+  all_points_w = []
+  for i in range(n_scans):
+    # ranges = np.load("test_ranges.npy")
+    ranges = lidar_ranges[:, i]
+    # valid1 = np.logical_and((ranges < 30),(ranges> 0.1))
+    valid1 = np.logical_and((ranges < range_max),(ranges > range_min))
   
-  # Lidar points in the sensor frame
-  points = np.column_stack((ranges*np.cos(angles), ranges*np.sin(angles)))
+    t_scan = lidar_stamps[i]
+    j = np.argmin(np.abs(encoder_t - t_scan))                                     # find nearest encoder pose to LiDAR scan
+    x_r = x[j]
+    y_r = y[j]
+    theta_r = encoder_theta[j]
   
-  # Convert from meters to cells
-  cells = np.floor((points - MAP['min']) / MAP['res']).astype(int)
-  
-  # Insert valid points in the map
-  valid2 = np.all((cells >= 0) & (cells < MAP['size']),axis=1)
-  MAP['map'][tuple(cells[valid1&valid2].T)] = 1
+    # Lidar points in the sensor/body frame in range 
+    points_b = np.column_stack((ranges[valid1]*np.cos(angles[valid1]), ranges[valid1]*np.sin(angles[valid1])))
+
+    # convert to world frame
+    R = np.array([[np.cos(theta_r), -np.sin(theta_r)], 
+                  [np.sin(theta_r),  np.cos(theta_r)]])
+    
+    points_w = (R @ points_b.T).T + np.array([x_r, y_r])                            # world frame
+    
+    all_points_w.append(points_w)
+    # Convert from meters to cells
+    cells = np.floor((points_w - MAP['min']) / MAP['res']).astype(int)
+    
+    # Insert valid points in the map
+    valid2 = np.all((cells >= 0) & (cells < MAP['size']),axis=1)
+    MAP['map'][tuple(cells[valid2].T)] = 1
 
   # Plot the Lidar points
+  all_points_w = np.vstack(all_points_w)
   fig1 = plt.figure()
-  plt.plot(points[:,0],points[:,1],'.k')
+  # plt.plot(points_w[:,0],points_w[:,1],'.k')
+  plt.scatter(all_points_w[:,0], all_points_w[:,1], 1, 'k')
   plt.xlabel('x')
   plt.ylabel('y')
-  plt.title('Lidar scan')
+  plt.title(f'Lidar scan of dataset {dataset}')
   plt.axis('equal')
+  plt.grid(True)
   
   # Plot the grid map
   fig2 = plt.figure()
   plot_map(MAP['map'],cmap='binary')
-  plt.title('Grid map')
+  plt.title(f'Grid map of dataset {dataset}')
+  plt.grid(True)
+  plt.axis('equal')
   
   plt.show()
+  return MAP
 
+
+
+
+def encoder_IMU_odometry(dataset = 20, d_tick = 0.0022, plot = False):
+    encoder_data, lidar_data, imu_data, kinect_data = load_dataset(dataset)
+    encoder_counts = encoder_data["encoder_counts"]
+    encoder_stamps = encoder_data["encoder_stamps"] # 40 Hz 
+    
+    imu_angular_velocity = imu_data["imu_angular_velocity"] 
+    imu_stamps = imu_data["imu_stamps"] # 100 Hz 
+    print("encoder stamps size", len(encoder_stamps)) 
+    print("imu stamps size", len(imu_stamps)) 
+    
+    # only use time range when both sensors are on 
+    t_start = max(encoder_stamps[0], imu_stamps[0]) 
+    t_end = min(encoder_stamps[-1], imu_stamps[-1]) 
+    print("\n\ntime range", t_start, "to ", t_end) 
+
+
+    imu_range = (imu_stamps >= t_start) & (imu_stamps <= t_end)
+    
+    imu_angular_velocity_yaw = imu_angular_velocity[2, imu_range]               # imu yaw only synced max start, min end
+    imu_stamps_yaw = imu_stamps[imu_range]                                      # sync up imu with average imu over longer encoder time steps since encoder is slower/less samples 
+
+    encoder_range = (encoder_stamps >= t_start) & (encoder_stamps <= t_end)     # encoder to max start, min end 
+    encoder_stamps_sync = encoder_stamps[encoder_range]
+    encoder_counts_sync = encoder_counts[:, encoder_range]
+
+    encoder_dt = np.diff(encoder_stamps_sync) 
+    
+    # encoder displacements 
+    d_R = (encoder_counts_sync[0,:] + encoder_counts_sync[2,:]) * d_tick/2                # (FR+RR)/2 * 0.0022m 
+    d_L = (encoder_counts_sync[1,:]+encoder_counts_sync[3,:]) * d_tick/2                  # (FL+RL)/2 * 0.0022m 
+    d_center = (d_R + d_L) / 2                                                  # distance traveled by robot center 
+    
+    v = d_center[1:]/encoder_dt # velocity at robot center 
+    
+    imu_dt = np.diff(imu_stamps_yaw) 
+    imu_theta_yaw = np.zeros(imu_angular_velocity_yaw.shape) 
+    imu_theta_yaw[1:] = np.cumsum((imu_angular_velocity_yaw[0:-1]+imu_angular_velocity_yaw[1:])/2 * imu_dt) # interporlate imu data instead of finding nearest points to encoder timestamp 
+    
+    encoder_theta = np.interp(encoder_stamps_sync, imu_stamps_yaw, imu_theta_yaw) # interpolate imu angular distance over encoder timestamp 
+    d_theta = np.diff(encoder_theta) 
+    omega = d_theta / encoder_dt 
+    
+    N = len(encoder_dt) 
+    x = np.zeros(N+1) 
+    y = np.zeros(N+1) 
+    # theta = np.zeros(N+1) # use abosulte, dead reckon the changes in orientation every step 
+    
+    # theta[1:] = np.cumsum(omega * encoder_dt) 
+    # for i in range(N):
+    #     x[i+1] = x[i] + d_center[i] * np.cos(encoder_theta[i]) 
+    #     y[i+1] = y[i] + d_center[i] * np.sin(encoder_theta[i]) 
+    
+    d_x = d_center[1:] * np.cos(encoder_theta[:-1]) # d_center = v * dt 
+    d_y = d_center[1:] * np.sin(encoder_theta[:-1]) 
+    
+    x[1:] = np.cumsum(d_x) 
+    y[1:] = np.cumsum(d_y)
+
+    plt.figure()
+    plt.plot(x, y)
+    plt.axis("equal")
+    plt.title(f"Odometry Trajectory (Encoder + IMU) of dataset {dataset}")
+    plt.xlabel("x (m)")
+    plt.ylabel("y (m)")
+    plt.grid(True)
+    plt.show()
+    if plot:
+        plt.figure()
+        plt.plot(encoder_stamps_sync, encoder_theta)
+        plt.title("Heading vs Time (Integrated IMU)")
+        plt.xlabel("time (s)")
+        plt.ylabel("theta (rad)")
+        plt.grid(True)
+        plt.show()
+
+
+        print("dt min:", encoder_dt.min(), "dt median:", np.median(encoder_dt))
+        print("v max:", v.max())
+        print("where v > 4 m/s:", np.sum(v > 4))
+
+        t_mid = 0.5 * (encoder_stamps_sync[:-1] + encoder_stamps_sync[1:])
+
+        plt.figure()
+        plt.plot(t_mid, v)
+        plt.title("Linear velocity v(t) from encoders")
+        plt.xlabel("time (s)")
+        plt.ylabel("v (m/s)")
+        plt.grid(True)
+        plt.show()
+
+        plt.figure()
+        plt.plot(t_mid, omega)
+        plt.title("Yaw rate ω(t) aligned to encoder intervals")
+        plt.xlabel("time (s)")
+        plt.ylabel("ω (rad/s)")
+        plt.grid(True)
+        plt.show()
+
+        dt = imu_dt
+        plt.figure()
+        plt.plot(dt)
+        plt.title("Encoder dt per step")
+        plt.xlabel("step k")
+        plt.ylabel("dt (s)")
+        plt.grid(True)
+        plt.show()
+
+        print("dt mean:", dt.mean(), "min:", dt.min(), "max:", dt.max())
+    return t_start, t_end, encoder_counts_sync, encoder_stamps_sync, v, imu_angular_velocity_yaw, imu_stamps_yaw, x, y, encoder_theta
 if __name__ == '__main__':
   show_lidar()
   test_bresenham2D()
